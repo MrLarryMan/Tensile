f256254928e28997b923fe85faf319f656b570e8
MrLarryMan <liu.larry@gmail.com>
2025-04-11 22:56:52 -0400

    Add Async Data Retrieval with JSON file as server

--- frontend/saved_jobs.js
+++ frontend/saved_jobs.js
@@ -1,14 +1,28 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", async function () {
     document.getElementById("details-box").style.display = "none";
-    const jobs = { // eventually replaced by a database
-        "test": { job_name: "Test_Job_1", url: "http://test_job_1", endpoint: "/api/sensors", test_options: { option1: true, option2: false}, datatype: "base64"},
-        "test2": { job_name: "Test_Job_2", url: "http://test_job_2", endpoint: "/api/app", test_options: { option1: true, option2: false}, datatype: "JSON"}
-    };
+    let jobs;
+    try {
+        jobs = await fetch('../data/saved_jobs.json').then(res => res.json());
+    } catch (error) {
+        throw new Error(`Error fetching from database ${error}`);
+    }
+
+    document.getElementById("jobselect").addEventListener('focus', function() {
+        document.getElementById("jobselect").innerHTML = '<option value=""></option>  <!-- Blank option -->';
+
+        console.log(jobs)
+        jobs.forEach(job => {
+            const opt = document.createElement('option');
+            opt.value = job.id;
+            opt.textContent = job.job_name;
+            document.getElementById("jobselect").appendChild(opt);
+        });
+    });
 
     document.getElementById("jobselect").addEventListener("change", function() {
         const job_id = this.value;
+        console.log(job_id);
         const details_div = document.getElementById("details-box");
-
         if (job_id && jobs[job_id]) {
             document.getElementById("job-name").innerText = jobs[job_id].job_name;
             document.getElementById("url").innerText = jobs[job_id].url;
