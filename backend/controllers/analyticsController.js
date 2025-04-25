dabfad8fe7ea59656d10072f8341186793f356db
Mason Lem <mlemberger@umass.edu>
2025-04-25 10:12:42 -0400

    Create the routes and most of the controller functions

--- backend/controllers/analyticsController.js
+++ backend/controllers/analyticsController.js
@@ -3,7 +3,7 @@ const { NotFoundError } = require('../errors/notFoundError');
 
 exports.getJobResults = async (req, res) => {
     try {
-        const jobResults = await jobResult.find({});
+        const jobResults = await jobResult.getAll();
         res.status(200).json(jobResults);
     } catch (error) {
         res.status(500).json({ message: 'Error fetching job results', error });
@@ -13,16 +13,31 @@ exports.getJobResults = async (req, res) => {
 exports.getJobResultById = async (req, res) => {
     const { id } = req.params;
     try {
-        const jobResult = await jobResult.findById(id);
+        const jobResult = await jobResult.getById(id);
         if (!jobResult) {
             throw new NotFoundError(`Job result with id ${id} not found`);
         }
+        res.status(200).json(jobResult);
     } catch (error) {
         if (error instanceof NotFoundError) {
             return res.status(404).json({ message: error.message });
         }
         res.status(500).json({ message: 'Error fetching job result', error });
     }
-    
-    res.status(200).json(jobResult);
+}
+
+exports.deleteJobResultById = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const deletedJobResult = await jobResult.deleteById(id);
+        if (!deletedJobResult) {
+            throw new NotFoundError(`Job result with id ${id} not found`);
+        }
+        res.status(200).json({ message: 'Job result deleted successfully' });
+    } catch (error) {
+        if (error instanceof NotFoundError) {
+            return res.status(404).json({ message: error.message });
+        }
+        res.status(500).json({ message: 'Error deleting job result', error });
+    }
 }
--- backend/routes/analyticsRoutes.js
+++ backend/routes/analyticsRoutes.js
@@ -2,4 +2,8 @@ const express = require('express');
 const router = express.Router();
 const analyticsController = require('../controllers/analyticsController');
 
-router.get('/')
+router.get('/', analyticsController.getJobResults);
+router.get('/:id', analyticsController.getJobResultById);
+router.delete('/:id', analyticsController.deleteJobResultById);
+
+module.exports = router;
